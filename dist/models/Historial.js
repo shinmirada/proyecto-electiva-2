b121070e49a5e4fbd0a5c5e296585a158eb52299
shinmirada <sinmiradakun@gmail.com>
2025-10-16 07:53:18 -0500

    compilacion ts

--- dist/models/Historial.js
+++ dist/models/Historial.js
@@ -1,14 +1,25 @@
 export class Historial {
     constructor() {
         this.registros = [];
+        this.CLAVE_STORAGE = "historial_conversiones";
+        this.cargarStorage();
+    }
+    cargarStorage() {
+        const datos = localStorage.getItem(this.CLAVE_STORAGE);
+        this.registros = datos ? JSON.parse(datos) : [];
+    }
+    guardarStorage() {
+        localStorage.setItem(this.CLAVE_STORAGE, JSON.stringify(this.registros));
     }
     agregar(conversion) {
         this.registros.push(conversion);
+        this.guardarStorage();
     }
     listar() {
         return this.registros;
     }
     limpiar() {
         this.registros = [];
+        this.guardarStorage();
     }
 }
